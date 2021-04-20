7004295948c55f0f80aa5a8cb68e4d64fdfebc72
lars-ruijs <60745347+lars-ruijs@users.noreply.github.com>
2021-04-20 15:51:05 +0200

    Cleaned code + comments (client)

--- public/js/script.js
+++ public/js/script.js
@@ -1,23 +1,30 @@
+// Connect to socket.io library
 const socket = io();
-console.log("Client script");
 
+// Get username and roomId
 const user = document.querySelector("span.username").textContent;
 const room = location.pathname.split("/")[2];
 
+// Containers for messages and the form
 const messageContainer = document.querySelector("section.messages");
 const formContainer = document.querySelector("section.form");
 
+// Images inside the hint container
 const hintImg1 = document.querySelector("section.hints div.imagecontainer figure:first-of-type img");
 const hintImg2 = document.querySelector("section.hints div.imagecontainer figure:nth-of-type(2) img");
 
+// Message form
 const guessingForm = document.querySelector("form[name='guessing']");
 
+// Emit new-user event (when script is loaded) with the userName and roomId
 socket.emit('new-user', { userName: user, roomId: room });
 
+// On userlist event > update round, usernames and points inside scoreboard
 socket.on('userlist', (dataObj) => {
   const scoreboard = document.querySelector("ul.scoreboard");
   const round = document.querySelector("aside section p:first-of-type");
 
+  // Remove old scoreboard (if present)
   if(scoreboard) {
     scoreboard.remove();
   }
@@ -26,21 +33,21 @@ socket.on('userlist', (dataObj) => {
   makeScoreBoard(dataObj);
 }); 
 
-socket.on('questionPicker', (data) => {
+// When a (new) question picker is set
+socket.on('questionPicker', () => {
 
-  if(guessingForm) {
-    guessingForm.classList.add("hide");
-  }
-
+  // Hide normal messages form > Make question picker form
+  guessingForm.classList.add("hide");
   makeQuestionForm();
 
 });
 
+// When a round is started by the question picker
 socket.on('start-round', (data) => {
-
   const questionPickerForm = document.querySelector("form[name='question']");
   const questionPickerInfo = document.querySelector("section.messages div.pickerInfo");
 
+  // Remove the question picker form and game information > Show the normal messages form > Add message
   if(questionPickerForm && questionPickerInfo){
     questionPickerInfo.remove();
     questionPickerForm.remove();
@@ -51,38 +58,43 @@ socket.on('start-round', (data) => {
     addMessage("round", `🏁 🏁 ${data.userName} has started the round. Start guessing now! 🏁 🏁`);
   }
 
+  // Set the hint images sources
   hintImg1.src = data.images[0];
   hintImg2.src = data.images[1];
 });
 
+// When the server sends a message
 socket.on('server-message', (messageObj) => {
 
+  // If it's a new question picker message (round-ended) > replace the hint images for the defaults
   if(messageObj.type === "newPicker") {
     hintImg1.src = "/images/guessimage.png";
     hintImg2.src = "/images/questionmarks.png";
   }
 
+  // Add message
   addMessage(messageObj.type, messageObj.message);
 }); 
 
-socket.on('error', (message) => {
-    addMessage("error", message);
-}); 
-
+// When a chat message was send
 socket.on('chat-message', (messageObj) => {
   addMessage("message", messageObj.message, messageObj.userName);
 });
 
+// When the game ends
 socket.on('game-ended', (scores) => {
 
+  // Sort the scores from highest to lowest
+  // Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
   scores.users.sort(function (a, b) {
     return b.points - a.points;
   });
 
-  const scoreScreen = document.querySelector("section.end-scores");
+  // Title with the username of the player who won
   const winnerTitle = document.querySelector("section.end-scores h2:nth-of-type(2)");
   winnerTitle.textContent = `${scores.users[0].userName} won the game!`;
 
+  // Add a final scorelist
   const scoreList = document.querySelector("section.end-scores ol");
   scores.users.forEach(user => {
     const li = document.createElement("li");
@@ -90,8 +102,12 @@ socket.on('game-ended', (scores) => {
     scoreList.appendChild(li);
   });
 
+  // Show the scorescreen overlay by setting the height to 100%
+  const scoreScreen = document.querySelector("section.end-scores");
   scoreScreen.style.height = "100%";
 
+  // Add confetti to the screen, using 'canvas-confetti'
+  // Source: https://www.npmjs.com/package/canvas-confetti#examples
   const myCanvas = document.createElement('canvas');
   scoreScreen.appendChild(myCanvas);
 
@@ -104,14 +120,14 @@ socket.on('game-ended', (scores) => {
   const end = Date.now() + duration;
 
   (function frame() {
-    // launch a few confetti from the left edge
+    // Launch confetti from the left edge
     myConfetti({
       particleCount: 7,
       angle: 60,
       spread: 75,
       origin: { x: 0 }
     });
-    // and launch a few from the right edge
+    // Launch from the right edge
     myConfetti({
       particleCount: 7,
       angle: 120,
@@ -119,13 +135,14 @@ socket.on('game-ended', (scores) => {
       origin: { x: 1 }
     });
 
-    // keep going until we are out of time
+    // Keep going until time ends
     if (Date.now() < end) {
       requestAnimationFrame(frame);
     }
   }());
 });
 
+// When a message gets send
 guessingForm.addEventListener('submit', (event) => {
   event.preventDefault();
   const message = document.querySelector("form[name='guessing'] input");
@@ -136,6 +153,7 @@ guessingForm.addEventListener('submit', (event) => {
 });
 
 
+// Make the scoreboard with usernames and scored points
 function makeScoreBoard(dataObj) {
   const container = document.querySelector("aside section");
 
@@ -143,7 +161,6 @@ function makeScoreBoard(dataObj) {
   ul.classList.add("scoreboard");
 
   dataObj.users.forEach((user, index) => {
-    console.log(index);
     const li = document.createElement("li");
     const div = document.createElement("div");
     
@@ -167,6 +184,7 @@ function makeScoreBoard(dataObj) {
   container.appendChild(ul);
 }
 
+// Make the question picker form
 function makeQuestionForm() {
   const form = document.createElement("form");
   form.setAttribute("name", "question");
@@ -185,12 +203,14 @@ function makeQuestionForm() {
   form.addEventListener("submit", questionAsked);
 }
 
+// Add messages to the messages container
 function addMessage(type, message, userName) {
   const div = document.createElement("div");
   div.classList.add(type);
 
   const p = document.createElement("p");
 
+  // If username is provided
   if(userName) {
     p.innerHTML = `<span>${userName}:</span> ${message}`;
   } else {
@@ -202,6 +222,7 @@ function addMessage(type, message, userName) {
   messageContainer.scrollTop = messageContainer.scrollHeight;
 }
 
+// Create a form input
 function createInput(type, placeholder, value, required) {
   const input = document.createElement("input");
   input.setAttribute("type", type);
@@ -221,6 +242,7 @@ function createInput(type, placeholder, value, required) {
   return input;
 }
 
+// When the question picker starts a round > get input values > emit event to server
 function questionAsked(event) {
   event.preventDefault();
   const subjectToGuess = document.querySelector("form[name='question'] input:first-of-type");
@@ -230,13 +252,4 @@ function questionAsked(event) {
   if(subjectToGuess.value && imgKeyword1.value && imgKeyword2.value) {    
     socket.emit('question-asked', { answer: subjectToGuess.value, hint1: imgKeyword1.value, hint2: imgKeyword2.value });
   }
-  console.log(subjectToGuess, imgKeyword1, imgKeyword2);
 }
-
-// document.querySelector('form').addEventListener('submit', (event) => {
-//     event.preventDefault();
-//     if (input.value) {
-//       socket.emit('message', input.value);
-//       input.value = '';
-//     }
-// });
